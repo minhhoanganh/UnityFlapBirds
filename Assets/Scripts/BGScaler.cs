86ffded4a29c28db2ed752d0d23ac3c1261c2e68
minhhoanganh <mrminhhoanganh@gmail.com>
2019-07-29 20:45:55 +0700

    first

---
+++ Assets/Scripts/BGScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent <SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+
+        float height = sr.bounds.size.y;
+        float width = sr.bounds.size.x;
+
+        float wordHeight = Camera.main.orthographicSize * 2f;
+        float wordWidth = wordHeight * Screen.width/Screen.height;
+
+        tempScale.y = wordHeight / height;
+        tempScale.x = wordWidth / width;
+
+        transform.localScale = tempScale;
+    }
+}
---
+++ Assets/Scripts/BirdsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsController : MonoBehaviour
+{
+    public float bounceForce;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip flyClip, pingClip, DeadClip;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        BirdsMoveMent();
+    }
+
+    void BirdsMoveMent()
+    {
+        myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+        audioSource.PlayOneShot(flyClip);
+
+
+        float angel = 0;
+        if (myBody.velocity.y > 0)
+        {
+            angel = Mathf.Lerp(0, 90, myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        }
+        else if (myBody.velocity.y == 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (myBody.velocity.y < 0)
+        {
+            angel = Mathf.Lerp(0, -90, -myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        }
+    }
+}
