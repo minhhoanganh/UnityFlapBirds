81236aa823cf962962d74d3b2d651d8f03050500
minhhoanganh <mrminhhoanganh@gmail.com>
2019-07-29 21:36:25 +0700

    done day 8_2

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PipeMoveMent();
+    }
+
+    void PipeMoveMent()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+
+        transform.position = temp;
+    }
+}
